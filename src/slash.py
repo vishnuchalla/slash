253b1feaf6b916af2aef1f96f21b870d96e0fe82
Vishnu Challa <vchalla2@ncsu.edu>
2022-10-07 15:50:54 -0400

    adding email in the argument parser

--- src/slash.py
+++ src/slash.py
@@ -37,6 +37,7 @@ def main():
     parser.add_argument('--des',
                         action='store_true',
                         help="Sort in descending (non-increasing) order")
+    parser.add_argument('--email', type=str, default="", help="list of email to get notified")
     args = parser.parse_args()
 
     products1 = scraper.searchAmazon(args.search, args.link)
@@ -46,14 +47,14 @@ def main():
         products1 = formatter.sortList(products1, sortBy, args.des)[:args.num]
         products2 = formatter.sortList(products2, sortBy, args.des)[:args.num]
         products3 = formatter.sortList(products3, sortBy, args.des)[:args.num]
-        results = products1 + products2 + products3
-        results = formatter.sortList(results, sortBy, args.des)
+        mergedLists = email_utils.alternateMerge(products1 + products2 + products3)
+        results = formatter.sortList(mergedLists, sortBy, args.des)
 
     print()
     print()
     print(tabulate(results, headers="keys", tablefmt="github"))
     print("\nWriting data to items.csv\n")
-    email_utils.write_data(results, args.link)
+    email_utils.write_data(results, args.email)
     print("Done :)")
     print()
     print()
