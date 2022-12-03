1174322f48bcb34d3706ca82a92ccd499e97cc6f
vamsitadikonda <vamsitadikonda99@gmail.com>
2022-12-03 18:14:56 -0500

    Removing duplicate methods

--- src/scraper.py
+++ src/scraper.py
@@ -31,28 +31,6 @@ def httpsGet(URL):
     }
     print(URL)
     page = requests.get(URL, headers=headers)
-    soup1 = BeautifulSoup(page.content, "html.parser")
-
-    return BeautifulSoup(soup1.prettify(), "html.parser")
-
-
-def httpsGet1(URL):
-    """
-    The httpsGet function makes HTTP called to the requested URL with custom headers
-    return: scraped html content from the URL
-    """
-    headers = {
-        "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept":
-            "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "DNT": "1",
-        "Connection": "close",
-        "Upgrade-Insecure-Requests": "1"
-    }
-    print(URL)
-    page = requests.get(URL, headers=headers)
     return page.content
 
 
