8a8b00ba79bc96353812260ce574fcfe5612559c
srujan ponnur <srujanponnur@gmail.com>
2022-12-04 02:35:05 +0000

    Automated autoyapf fixes

--- src/slash.py
+++ src/slash.py
@@ -18,15 +18,44 @@ from threading import Thread
 
 def extractProducts(args):
     result_queue = Queue()
-    search = args.search if isinstance(args, argparse.Namespace) else args['search']
-    link = bool(args.link) if isinstance(args, argparse.Namespace) else bool(args.get('link', True))
-    num = int(args.num) if isinstance(args, argparse.Namespace) else int(args.get('num', 3))
-    sort = args.sort if isinstance(args, argparse.Namespace) else args.get('sort', "re")
-    des = bool(args.des) if isinstance(args, argparse.Namespace) else bool(args.get('des', True))
+    search = args.search if isinstance(args,
+                                       argparse.Namespace) else args['search']
+    link = bool(args.link) if isinstance(args, argparse.Namespace) else bool(
+        args.get('link', True))
+    num = int(args.num) if isinstance(args, argparse.Namespace) else int(
+        args.get('num', 3))
+    sort = args.sort if isinstance(args, argparse.Namespace) else args.get(
+        'sort', "re")
+    des = bool(args.des) if isinstance(args, argparse.Namespace) else bool(
+        args.get('des', True))
 
-    t1 = Thread(target=scraper.searchAmazon, args=(search, link, result_queue, num,), )
-    t2 = Thread(target=scraper.searchWalmart, args=(search, link, result_queue, num,), )
-    t3 = Thread(target=scraper.searchTarget, args=(search, link, result_queue, num,), )
+    t1 = Thread(
+        target=scraper.searchAmazon,
+        args=(
+            search,
+            link,
+            result_queue,
+            num,
+        ),
+    )
+    t2 = Thread(
+        target=scraper.searchWalmart,
+        args=(
+            search,
+            link,
+            result_queue,
+            num,
+        ),
+    )
+    t3 = Thread(
+        target=scraper.searchTarget,
+        args=(
+            search,
+            link,
+            result_queue,
+            num,
+        ),
+    )
 
     # Starting threads...
     t1.start()
@@ -47,8 +76,10 @@ def extractProducts(args):
 
 
 def send_email(results, args):
-    email = args.email if isinstance(args, argparse.Namespace) else args.get('email', "")
-    link = bool(args.link) if isinstance(args, argparse.Namespace) else bool(args.get('link', True))
+    email = args.email if isinstance(args, argparse.Namespace) else args.get(
+        'email', "")
+    link = bool(args.link) if isinstance(args, argparse.Namespace) else bool(
+        args.get('link', True))
     try:
         if email:
             email_utils.write_data(results, link, email)
@@ -65,9 +96,7 @@ def main():
     parser = argparse.ArgumentParser(description="Slash")
     parser.add_argument('--search', type=str, help='Product search query')
     parser.add_argument('--num',
-                        type=int
-
-                        ,
+                        type=int,
                         help="Maximum number of records",
                         default=3)
     parser.add_argument(
@@ -94,7 +123,8 @@ def main():
     print(tabulate(results, headers="keys", tablefmt="github"))
     send_email(results, args)
     end_time = datetime.datetime.now()
-    print("t1:{},t1:{}, diff = {}".format(start_time, end_time, end_time - start_time))
+    print("t1:{},t1:{}, diff = {}".format(start_time, end_time,
+                                          end_time - start_time))
 
 
 if __name__ == '__main__':
