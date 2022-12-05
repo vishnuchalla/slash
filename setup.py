224f6c93ca37721a28adc429dbf5c9228e1672fa
saisreenalluru <saisree4444@gmail.com>
2022-12-05 12:37:46 -0500

    Updating Email address at every instance(#42)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Copyright (C) 2022 SE Slash - All Rights Reserved
 You may use, distribute and modify this code under the
 terms of the MIT license.
 You should have received a copy of the MIT license with
-this file. If not, please write to: secheaper@gmail.com
+this file. If not, please write to: vamsitadikonda99@gmail.com
 
 """
 
--- src/api/scraper.py
+++ src/api/scraper.py
@@ -3,7 +3,7 @@ Copyright (C) 2021 SE Slash - All Rights Reserved
 You may use, distribute and modify this code under the
 terms of the MIT license.
 You should have received a copy of the MIT license with
-this file. If not, please write to: vamsitadikonda@gmail.com
+this file. If not, please write to: vamsitadikonda99@gmail.com
 
 The scraper module holds functions that actually scrape the e-commerce websites
 """
